f1a321ea6b4f1a25fe02a0676ee79071846d09eb
ricklarios <81334604+ricklarios@users.noreply.github.com>
2022-05-31 01:19:53 +0200

    ImageView

--- ViewsAndNavigationControls/ViewController.swift
+++ ViewsAndNavigationControls/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
 	@IBOutlet weak var myView: UIView!
+	@IBOutlet weak var myImage: UIImageView!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
