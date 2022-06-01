fbb9556d6e9b21ccf945ef9e4b2822449b07b186
ricklarios <81334604+ricklarios@users.noreply.github.com>
2022-06-02 01:43:02 +0200

    WebView

--- ViewsAndNavigationControls/ViewController.swift
+++ ViewsAndNavigationControls/ViewController.swift
@@ -6,15 +6,20 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
-	@IBOutlet weak var myView: UIView!	
+	@IBOutlet weak var myView: UIView!
 	@IBOutlet weak var myFakeView: UIView!
 	@IBOutlet weak var myImageView: UIImageView!
+	@IBOutlet weak var myWebView: WKWebView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		
+		myWebView.load(URLRequest(url: URL(string: "https://google.es")!))
 	}
 
 	@IBAction func myButtonAction(_ sender: Any) {
