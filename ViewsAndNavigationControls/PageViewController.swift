af423da394f2a23a33dfd83ef9f3347428f3273b
ricklarios <81334604+ricklarios@users.noreply.github.com>
2022-06-03 00:18:33 +0200

    Add comments to Page View controllers

--- ViewsAndNavigationControls/PageViewController.swift
+++ ViewsAndNavigationControls/PageViewController.swift
@@ -9,29 +9,36 @@ import UIKit
 
 class PageViewController: UIPageViewController {
 	
+	// Creo un array al que añadiré cada VC
 	private var myControllers: [UIViewController] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+		// Creo un UIStoryboard con el VC identificado como vcOrange
 		let myOrangeVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "vcOrange")
-		
+		// Creo un UIStoryboard con el VC identificado como vcPurple
 		let myPurpleVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "vcPurple")
 		
+		// Añado mis vistas al array de VC
 		myControllers.append(myOrangeVC)
 		myControllers.append(myPurpleVC)
 		
-		setViewControllers([myOrangeVC], direction: .forward, animated: true, completion: nil) // Establezco cual va a ser el primer view controller que veamos
+		// Establezco cual va a ser el primer VC que veamos
+		setViewControllers([myOrangeVC], direction: .forward, animated: true, completion: nil)
 		
+		// Le indico que el protocolo dataSource está definido dentro de la clase (extension)
 		dataSource = self
 		
     }
     
 }
 
+// Defino por extensión el protocolo dataSource donde le indicamos qué hacer si vamos pasando páginas
 extension PageViewController: UIPageViewControllerDataSource {
+	
+	// Si vamos atrás
 	func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-		// Establecemos qué pasa si vamos atrás
 		let index = myControllers.firstIndex(of: viewController)
 		if index == 0 {
 			return myControllers.last
@@ -39,9 +46,9 @@ extension PageViewController: UIPageViewControllerDataSource {
 		return myControllers.first
 	}
 	
+	// Si vamos adelante
 	func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-		// Establecemos qué pasa si vamos adelante
-		let index = myControllers.firstIndex(of: viewController)
+				let index = myControllers.firstIndex(of: viewController)
 		if index == 0 {
 			return myControllers.last
 		}
